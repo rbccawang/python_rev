a6facbcdb31b1e304505bf56fdf440da3144dcfc
Rebecca Wang <e.rebeccawang@gmail.com>
2021-06-09 15:36:31 -0700

    day 2

---
+++ two.py
@@ -0,0 +1,21 @@
+# function is defined using def
+def my_goats():
+    print("goat farms are highly legal in goatopia")
+
+# call function
+my_goats()
+
+# Any input parameters or arguments should be placed within these parentheses. You can also define parameters inside these parentheses.
+# arguments (args): info in functions that are separated by commas
+# value that is sent to the function when it is called.
+# number of arguments = number of arguments you MUST exactly call
+def my_pink_goats(first, last):
+    print("blue " + first + " " + last)
+
+my_pink_goats("mimi", "wang")
+my_pink_goats("flubber", "wang")
+my_pink_goats("kisy", "wang")
+my_pink_goats("lols", "wang")
+
+# parameter: info in functions that are separated by commas
+# variable listed inside the parentheses in the function definition.
