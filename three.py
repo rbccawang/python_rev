8d78e487c1f70316e259b31150e63be7cbfa85ce
Rebecca Wang <e.rebeccawang@gmail.com>
2021-06-10 14:46:48 -0700

    day 3

--- three.py
+++ three.py
@@ -3,7 +3,16 @@
 # package: collection of related modules (dateutil, email, json)
 # Python Standard Library
  
-help(print)
-help(1000)
-
 # use python's math library
+
+import math
+
+x1 = 0.0
+y1 = 0.0
+x2 = 3.0
+y2 = 4.0
+
+# calculate the euclidian distance / distance between (x1,y1) and (x2,y2)
+distance = math.dist((x1,y1), (x2,y2))
+
+print ("distance between the two points: ", distance)
