ec429ade2f9b15d60ce7b1fb60362c464e499c40
Rebecca Wang <73043927+rebeccawang06@users.noreply.github.com>
2021-06-10 14:08:57 -0700

    Create three.py
    day three

---
+++ three.py
@@ -0,0 +1,4 @@
+# script: file intended to be ran directly and contains statements outside the scope of any class or function
+# module: files meant to be imported into other modules or scripts. defines classes, variables, and functions
+# package: collection of related modules (dateutil, email, json)
+# Python Standard Library
